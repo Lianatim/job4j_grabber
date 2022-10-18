4614677369bdb64481c953ca1410b5602c6492bd
Liana <95958723+Lianatim@users.noreply.github.com>
2022-10-18 23:22:05 +0300

    2.1. Преобразование даты

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -5,9 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-mm-ddТhh:mm:ss");
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse, DateTimeFormatter.ofPattern("yyyy-mm-ddТhh:mm:ss"));
+        return LocalDateTime.parse(parse, DATE_TIME_FORMATTER);
     }
 
 }
